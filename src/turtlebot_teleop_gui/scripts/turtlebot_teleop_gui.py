514d99f72f98372b1f19641988fedd1dc4dce883
Michael <michael.tal@protonmail.ch>
2020-12-16 15:00:34 +0100

    changed ros node to a class

--- src/turtlebot_teleop_gui/scripts/turtlebot_teleop_gui.py
+++ src/turtlebot_teleop_gui/scripts/turtlebot_teleop_gui.py
@@ -1,3 +1,7 @@
+"""
+This package is a GUI for the turtlebot_teleop package.
+"""
+
 import sys
 import os
 
@@ -7,20 +11,33 @@ from std_msgs.msg import String
 from PySide2.QtQml import QQmlApplicationEngine
 from PySide2.QtGui import QGuiApplication
 
-def callback(data):
-    rospy.loginfo(rospy.get_caller_id() + "I heard %s", data.data)
+class RosNode():
+    """
+    ROS Node Class
+    """
+    def callback(self, data):
+        """
+        Callback function for the subscriber.
+        """
+        rospy.loginfo(rospy.get_caller_id() + "I heard %s", data.data)
 
-def listener():
-    rospy.init_node('listener', anonymous=True)
+    def run(self):
+        """
+        Listener function for the ROS-Node
+        """
+        rospy.init_node('listener', anonymous=True)
 
-    rospy.Subscriber("chatter", String, callback)
+        rospy.Subscriber("chatter", String, self.callback)
+
+        #rospy.spin()
 
 if __name__ == "__main__":
     app = QGuiApplication(sys.argv)
     engine = QQmlApplicationEngine()
-    engine.load(os.path.join(os.path.dirname(__file__), "main.qml"))
+    engine.load(os.path.join(os.path.dirname(__file__), "ui/main.qml"))
 
-    listener()
+    n = RosNode()
+    n.run()
 
     if not engine.rootObjects():
         sys.exit(-1)
